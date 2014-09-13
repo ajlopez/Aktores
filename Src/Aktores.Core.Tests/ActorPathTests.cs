4280a546cff5483cfda69de047023841aaee11f2
ajlopez <webmaster@ajlopez.com>
2014-09-13 11:35:24 -0300

    ActorPath, IsName IsRelative first tests

---
+++ Src/Aktores.Core.Tests/ActorPathTests.cs
@@ -0,0 +1,44 @@
+﻿namespace Aktores.Core.Tests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ActorPathTests
+    {
+        [TestMethod]
+        public void IsName()
+        {
+            Assert.IsTrue(ActorPath.IsName(null));
+            Assert.IsTrue(ActorPath.IsName(string.Empty));
+            Assert.IsTrue(ActorPath.IsName("  "));
+
+            Assert.IsTrue(ActorPath.IsName("foo"));
+
+            Assert.IsFalse(ActorPath.IsName("./foo"));
+            Assert.IsFalse(ActorPath.IsName("/foo"));
+            Assert.IsFalse(ActorPath.IsName("//sys/foo"));
+            Assert.IsFalse(ActorPath.IsName("aktores://sys/foo"));
+            Assert.IsFalse(ActorPath.IsName("actores.tcp://sys@localhost:3000/foo"));
+        }
+
+        [TestMethod]
+        public void IsRelative()
+        {
+            Assert.IsTrue(ActorPath.IsRelative(null));
+            Assert.IsTrue(ActorPath.IsRelative(string.Empty));
+            Assert.IsTrue(ActorPath.IsRelative("  "));
+
+            Assert.IsTrue(ActorPath.IsRelative("foo"));
+
+            Assert.IsTrue(ActorPath.IsRelative("./foo"));
+            Assert.IsFalse(ActorPath.IsRelative("/foo"));
+            Assert.IsFalse(ActorPath.IsRelative("//sys/foo"));
+            Assert.IsFalse(ActorPath.IsRelative("aktores://sys/foo"));
+            Assert.IsFalse(ActorPath.IsRelative("actores.tcp://sys@localhost:3000/foo"));
+        }
+    }
+}
---
+++ Src/Aktores.Core/ActorPath.cs
@@ -0,0 +1,32 @@
+﻿namespace Aktores.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class ActorPath
+    {
+        public static bool IsName(string path)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+                return true;
+
+            if (path.Contains("/"))
+                return false;
+
+            return true;
+        }
+
+        public static bool IsRelative(string path)
+        {
+            if (IsName(path))
+                return true;
+
+            if (path.StartsWith("."))
+                return true;
+
+            return false;
+        }
+    }
+}
