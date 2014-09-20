28123be2ab26a207d1b87ce4aa07996c48e0bd91
ajlopez <webmaster@ajlopez.com>
2014-09-20 09:05:48 -0300

    Address with first tests

---
+++ Src/Aktores.Core.Tests/AddressTests.cs
@@ -0,0 +1,34 @@
+﻿namespace Aktores.Core.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class AddressTests
+    {
+        [TestMethod]
+        public void SplitInFullPathInParts()
+        {
+            var path = new Address("aktores.tcp://sys@localhost:3000");
+
+            Assert.AreEqual("aktores.tcp", path.Protocol);
+            Assert.AreEqual("sys", path.SystemName);
+            Assert.AreEqual("localhost", path.HostName);
+            Assert.AreEqual(3000, path.Port);
+        }
+
+        [TestMethod]
+        public void SplitPartialPathInParts()
+        {
+            var path = new Address("//sys2");
+
+            Assert.AreEqual("aktores", path.Protocol);
+            Assert.AreEqual("sys2", path.SystemName);
+            Assert.IsNull(path.HostName);
+            Assert.AreEqual(0, path.Port);
+        }
+    }
+}
---
+++ Src/Aktores.Core/Address.cs
@@ -0,0 +1,63 @@
+﻿namespace Aktores.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Address
+    {
+        private string protocol;
+        private string systemname;
+        private string hostname;
+        private int port;
+
+        public Address(string path)
+        {
+            int p = path.IndexOf(':');
+
+            if (p > 0)
+            {
+                this.protocol = path.Substring(0, p);
+                path = path.Substring(p + 1);
+            }
+            else
+                this.protocol = "aktores";
+
+            if (path.StartsWith("//"))
+            {
+                string location = path.Substring(2);
+
+                p = location.IndexOf("@");
+
+                if (p > 0)
+                {
+                    this.systemname = location.Substring(0, p);
+                    location = location.Substring(p + 1);
+
+                    p = location.IndexOf(":");
+
+                    if (p > 0)
+                    {
+                        this.hostname = location.Substring(0, p);
+                        this.port = int.Parse(location.Substring(p + 1));
+                    }
+                    else
+                        this.hostname = location;
+                }
+                else
+                    this.systemname = location;
+            }
+            else
+                this.systemname = "sys";
+        }
+
+        public string Protocol { get { return this.protocol; } }
+
+        public string SystemName { get { return this.systemname; } }
+
+        public string HostName { get { return this.hostname; } }
+
+        public int Port { get { return this.port; } }
+    }
+}
