6beb4170e7c4e5bbec325af40fd44ecc7a4173a7
ajlopez <webmaster@ajlopez.com>
2014-10-04 10:33:51 -0300

    RouterActor with many children

--- Src/Aktores.Core.Tests/Routing/RouterActorTests.cs
+++ Src/Aktores.Core.Tests/Routing/RouterActorTests.cs
@@ -11,26 +11,50 @@
         [TestMethod]
         public void CreateRouterAndSendMessageToActor()
         {
-            int total = 0;
-            EventWaitHandle wait = new AutoResetEvent(false);
-
-            Actor actor = new LambdaActor(c => { total += (int)c; wait.Set(); });
-
             ActorSystem system = new ActorSystem();
 
+            int total = 0;
+            EventWaitHandle wait = new AutoResetEvent(false);
+            Actor actor = new LambdaActor(c => { total += (int)c; wait.Set(); });
             var actorref = system.ActorOf(actor, "myactor");
 
             var router = new RouterActor();
-
             router.Register(actorref);
-
             var routerref = system.ActorOf(router, "myrouter");
 
             routerref.Tell(3);
 
             Assert.IsTrue(wait.WaitOne(1000));
-
             Assert.AreEqual(3, total);
+
+            system.Shutdown();
+        }
+
+        [TestMethod]
+        public void CreateRouterAndSendMessageToActors()
+        {
+            ActorSystem system = new ActorSystem();
+
+            int total = 0;
+            EventWaitHandle wait = new AutoResetEvent(false);
+
+            var router = new RouterActor();
+            var routerref = system.ActorOf(router, "myrouter");
+
+            for (int k = 0; k < 100; k++)
+            {
+                Actor actor = new LambdaActor(c => { lock (system) { total += (int)c; } if (total >= 10) wait.Set(); });
+                var actorref = system.ActorOf(actor);
+                router.Register(actorref);
+            }
+
+            routerref.Tell(1);
+            routerref.Tell(2);
+            routerref.Tell(3);
+            routerref.Tell(4);
+
+            Assert.IsTrue(wait.WaitOne(1000));
+            Assert.AreEqual(10, total);
 
             system.Shutdown();
         }
