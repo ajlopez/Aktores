f1c6938b4b8b7e2e6b323649b1d7982cbcb3e67e
ajlopez <webmaster@ajlopez.com>
2014-10-04 10:26:10 -0300

    RouterActor first test

---
+++ Src/Aktores.Core.Tests/Routing/RouterActorTests.cs
@@ -0,0 +1,38 @@
+﻿namespace Aktores.Core.Tests.Routing
+{
+    using System;
+    using System.Threading;
+    using Aktores.Core.Routing;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class RouterActorTests
+    {
+        [TestMethod]
+        public void CreateRouterAndSendMessageToActor()
+        {
+            int total = 0;
+            EventWaitHandle wait = new AutoResetEvent(false);
+
+            Actor actor = new LambdaActor(c => { total += (int)c; wait.Set(); });
+
+            ActorSystem system = new ActorSystem();
+
+            var actorref = system.ActorOf(actor, "myactor");
+
+            var router = new RouterActor();
+
+            router.Register(actorref);
+
+            var routerref = system.ActorOf(router, "myrouter");
+
+            routerref.Tell(3);
+
+            Assert.IsTrue(wait.WaitOne(1000));
+
+            Assert.AreEqual(3, total);
+
+            system.Shutdown();
+        }
+    }
+}
---
+++ Src/Aktores.Core/Routing/RouterActor.cs
@@ -0,0 +1,24 @@
+﻿namespace Aktores.Core.Routing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class RouterActor : Actor
+    {
+        private Random random = new Random();
+        private IList<ActorRef> childrefs = new List<ActorRef>();
+
+        public void Register(ActorRef childref)
+        {
+            this.childrefs.Add(childref);
+        }
+
+        public override void Receive(object message)
+        {
+            var n = this.random.Next(this.childrefs.Count);
+            this.childrefs[n].Tell(message, this.Sender);
+        }
+    }
+}
