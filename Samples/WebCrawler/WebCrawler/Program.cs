bbfc8ad09d410f352401c9c35bd3e2d625899591
ajlopez <webmaster@ajlopez.com>
2014-10-05 10:29:33 -0300

    WebCrawler using router

--- Samples/WebCrawler/WebCrawler/Program.cs
+++ Samples/WebCrawler/WebCrawler/Program.cs
@@ -6,6 +6,7 @@
     using System.Text;
     using System.Threading.Tasks;
     using Aktores.Core;
+    using Aktores.Core.Routing;
 
     public class Program
     {
@@ -14,15 +15,26 @@
             ActorSystem system = new ActorSystem();
 
             Resolver resolver = new Resolver();
-            Downloader downloader = new Downloader();
+            //Downloader downloader = new Downloader();
             Harvester harvester = new Harvester();
 
+            RouterActor router = new RouterActor();
+
             ActorRef resolverref = system.ActorOf(resolver, "resolver");
-            ActorRef downloaderref = system.ActorOf(downloader, "downloader");
+            //ActorRef downloaderref = system.ActorOf(downloader, "downloader");
+            ActorRef downloaderref = system.ActorOf(router, "downloader");
             ActorRef harvesterref = system.ActorOf(harvester, "harvester");
 
+            for (int k = 1; k <= 10; k++)
+            {
+                Downloader downloader = new Downloader();
+                downloader.Harvester = harvesterref;
+                ActorRef dlref = system.ActorOf(downloader);
+                router.Register(dlref);
+            }
+
             resolver.Downloader = downloaderref;
-            downloader.Harvester = harvesterref;
+            //downloader.Harvester = harvesterref;
             harvester.Resolver = resolverref;
 
             foreach (var arg in args)
